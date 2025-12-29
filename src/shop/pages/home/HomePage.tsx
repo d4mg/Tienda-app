3b34eadfa2d71e6cad0aaa44a56b5a6ff61cdd59
d4mg <diegoamarin2498@gmail.com>
2025-12-29 14:24:44 -0500

    Paginación del HomePage, Parámetros por url y sidebar

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { products } from '../../../mocks/products.mock';
+import { ProductGrid } from "@/shop/components/ProductGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" subTitle="Ropa minimalista y elegante inspirada en el diseño futurista de Tesla. Calidad premium para un estilo atemporal." />
+      
+      <ProductGrid products={products}/>
+      
+      <CustomPagination totalPages={7}/>
     </>
   )
 }
