dbea4f91fe52212ca251835f711214d70d43b984
d4mg <diegoamarin2498@gmail.com>
2025-12-29 17:52:00 -0500

    Navegación entre pantallas y logo personalizado

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,27 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductGrid } from "@/shop/components/ProductGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+  const {gender} = useParams();
+
+  const genderLabel = gender === 'men'
+    ?  'Hombres'
+    : gender === 'women'
+      ? 'Mujeres'
+      : 'Niños';
+
   return (
-    <div>GenderPage</div>
+    <>
+          <CustomJumbotron title={`Productos para ${genderLabel}`} subTitle="Ropa minimalista y elegante inspirada en el diseño futurista de Tesla. Calidad premium para un estilo atemporal." />
+          
+          <ProductGrid products={products}/>
+          
+          <CustomPagination totalPages={7}/>
+    
+    </>
   )
 }
